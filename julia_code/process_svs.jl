f0e5bfe2c4730b730884a771b84efb2d0843c9ba
opterix <daedrobu87@gmail.com>
2016-09-09 02:09:35 -0500

    Process svs convertido a funcion

--- julia_code/process_svs.jl
+++ julia_code/process_svs.jl
@@ -1,10 +1,21 @@
+function process_svs(filepath)
+    a=readcsv(filepath) #read file
+    
+    time=a[3:end,1]
+    b=a[3:end, 2:5] #Extract one channel information
+    
+    f(x) = typeof(x)==Float64 #function to test if elements are float
+    
+    bool_floats = broadcast(f,b)
+    bool_floats = convert(Array{Bool,2}, bool_floats) #Convert array to boolean appropiate for logical indexing
+    b[~bool_floats]=NaN #Convert all non-number strings to NaN
+    b=convert(Array{Float64,2}, b) #Convert to Float array
 
-a=readcsv("../data/a01.csv") #read file
-b=a[3:end, 1] #Extract one channel information
+    return time, b
+end
 
-f(x) = typeof(x)==Float64 #function to test if elements are float
-
-bool_floats = broadcast(f,b)
-bool_floats = convert(Array{Bool,1}, bool_floats) #Convert array to boolean appropiate for logical indexing
-b[~bool_floats]=NaN #Convert all non-number strings to NaN
-b=convert(Array{Float64,1}, b) #Convert to Float array
+# Ejemplo de uso:
+# filepath="../data/a01.csv"
+# (ecgtime,ecg)=process_svs(filepath)
+# #ecgtime-> tiempo ecg
+# #ecg -> Matriz de 60000x4
