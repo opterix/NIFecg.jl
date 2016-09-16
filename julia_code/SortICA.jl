d87142ccc1946d4e0a62bb0dc8ac4660344a1df6
jaruanob <josue.ruanob@gmail.com>
2016-09-16 11:03:25 -0500

    Function to sort the ICA components

---
+++ julia_code/SortICA.jl
@@ -0,0 +1,16 @@
+function SortICA(AECG_white)
+
+variance = zeros(m)
+  variance[1]=var(AECG_white[1,:])
+  variance[2]=var(AECG_white[2,:])
+  variance[3]=var(AECG_white[3,:])
+  variance[4]=var(AECG_white[4,:])
+  println("Variance_components=$variance")
+
+#  for
+
+#AECG_sort =
+
+#return
+
+end
