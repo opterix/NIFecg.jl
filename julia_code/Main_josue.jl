36be4661d292b025c6a5e75dd27b9b167c91d00f
jaruanob <josue.ruanob@gmail.com>
2016-09-19 16:01:12 -0500

    Giving order and adding sort function

--- julia_code/Main_josue.jl
+++ julia_code/Main_josue.jl
@@ -4,27 +4,28 @@ cd("/home/jarb/NI-Fecg/julia_code")
 include("process_svs.jl")
 include("MakeICAll.jl")
 include("SortICA.jl")
+include("Plotting.jl")
+
 
 ############# SOURCES #######################
 filepath="../data/a01.csv"
 #data = readtable("set-a/set-a-text/a01.csv") # data
 #t = linspace(0.0, 60.0, n) # time vector
 
-############ LOAD DATA ######################
 
+############ LOAD DATA ######################
 #----------- Read and fix data --------------
 (t,AECG) = process_svs(filepath)
 (n,m) = size(AECG) # m - number of electros, n - sample size
 
 
 ########## SOURCE SEPARATION ################
-
 #----------------- ICA ----------------------
-#(AECG_white, AECG_nowhite) = MakeICAIndep(AECG)
 k = 4 # number of components
-PyPlot.close("all")
 (AECG_white) = MakeICAll(AECG)
+#----------------- Sort ICA results ----------------------
+(AECG_sort)=SortICA(AECG_white)
 
-SortICA(AECG_white)
 
-#----------------- FFT ----------------------
+############### Plotting ###################
+Plotting(AECG,AECG_white,AECG_sort)
