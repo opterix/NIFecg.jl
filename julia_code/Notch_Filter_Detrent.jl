7202a854383ca80ce58df61ac6560502c38e4ea4
opterix <daedrobu87@gmail.com>
2016-09-20 12:47:44 -0500

    Filtering code modified and optimized. Now the notch and the lowpass filtering are applied

--- julia_code/Notch_Filter_Detrent.jl
+++ julia_code/Notch_Filter_Detrent.jl
@@ -5,24 +5,37 @@
 using DSP, MultivariateStats, PyPlot
 
 function notch_filter(x)
-x=ecg; #read matrix ecg
-Frec_Mues=1000;
+    #x=ecg; #read matrix ecg
+    Frec_Mues=1000;
+    time=60
+    #plot(ecgtime,signals[:,1]);
+    nmuestras=time*Frec_Mues
+    
+    #title("Pure ECG")
 
-plot(ecgtime,signals[:,1]);
-title("Pure ECG")
+    responsetype = Lowpass(3,fs=Frec_Mues);
+    prototype=Butterworth(8);
+    Notch=iirnotch(60,1,fs=Frec_Mues)
+    detrend_filter= digitalfilter(responsetype, prototype);
 
-responsetype = Lowpass(3,fs=1000);
-prototype=Butterworth(8);
+    #sf=Array{Float64}(nmuestras, 4)
 
-Notch=iirnotch(60,1,fs=1000)
-EMG= digitalfilter(responsetype, prototype);
-for i=0;i<4;i++
-sf= filtfilt (Notch, x [:,i])
-plot(ecgtime,sf);
+    #detrending -> No hay necesidad de hacerlo por canal, se puede pasar directamente la matriz a filtfilt
+    #for i=1;i<5;i++
+    #    sf = filtfilt(detrend_filter, x[:,1])
+        #plot(ecgtime,sf);
+    #end
+
+    lowSignal=filtfilt(detrend_filter, x)
+    sf= x - lowSignal
+
+    sf = filtfilt(Notch, sf)
+
+    #notchfiltering-> Tampoco hay necesidad de hacer el notch por canal
+    #for i=1;i<5;i++
+    #    sf = filtfilt(Notch, x[:,i])
+        #plot(ecgtime,sf);
+    #end
+    return sf, lowSignal
+
 end
-
-#    return ecg, Notch
-
-end
-
-
