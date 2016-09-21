28b6a870c9ebd96ac66005ae22d24ea0810c861d
jaruanob <josue.ruanob@gmail.com>
2016-09-21 16:07:41 -0500

    We can choose how many seconds and components want to plot

--- julia_code/Plotting.jl
+++ julia_code/Plotting.jl
@@ -1,44 +1,29 @@
 function Plotting(AECG,AECG_white,AECG_sort)
 
 # --- Size of window to plot
-win=10000
+win=seconds*1000
 
 PyPlot.close("all")
 
 figure(1)
-
-subplot(411)
+for ind in 1:m
+subplot("41$(ind)")
 title("AECG data")
- plot(t[1:win], AECG[1:win,1], color="red", linewidth=1.0, linestyle="-")
-subplot(412)
- plot(t[1:win], AECG[1:win,2], color="red", linewidth=1.0, linestyle="-")
-subplot(413)
- plot(t[1:win], AECG[1:win,3], color="red", linewidth=1.0, linestyle="-")
-subplot(414)
- plot(t[1:win], AECG[1:win,4], color="red", linewidth=1.0, linestyle="-")
+ plot(t[1:win], AECG[1:win,ind], color="red", linewidth=1.0, linestyle="-")
+end
 
 figure(2)
-
-subplot(411)
-title("ICA_white")
-plot(t[1:win], AECG_white[1:win,1], color="red", linewidth=1.0, linestyle="-")
-subplot(412)
-plot(t[1:win], AECG_white[1:win,2], color="black", linewidth=1.0, linestyle="-")
-subplot(413)
-plot(t[1:win], AECG_white[1:win,3], color="blue", linewidth=1.0, linestyle="-")
-subplot(414)
-plot(t[1:win], AECG_white[1:win,4], color="blue", linewidth=1.0, linestyle="-")
+for ind in 1:k
+  subplot("$(k)1$(ind)")
+  title("ICA_white $k")
+  plot(t[1:win], AECG_white[1:win,ind], color="black", linewidth=1.0, linestyle="-")
+end
 
 figure(3)
-
-subplot(411)
-title("ICA_sort")
-plot(t[1:win], AECG_sort[1:win,1], color="red", linewidth=1.0, linestyle="-")
-subplot(412)
-plot(t[1:win], AECG_sort[1:win,2], color="black", linewidth=1.0, linestyle="-")
-subplot(413)
-plot(t[1:win], AECG_sort[1:win,3], color="blue", linewidth=1.0, linestyle="-")
-subplot(414)
-plot(t[1:win], AECG_sort[1:win,4], color="blue", linewidth=1.0, linestyle="-")
+for ind in 1:k
+  subplot("$(k)1$(ind)")
+title("ICA_white")
+plot(t[1:win], AECG_sort[1:win,ind], color="blue", linewidth=1.0, linestyle="-")
+end
 
 end
