d0f60412a3a5be39771cdc2ec7521f6597a1f09d
jaruanob <josue.ruanob@gmail.com>
2016-09-21 16:06:09 -0500

    Improve order

--- julia_code/Main_josue.jl
+++ julia_code/Main_josue.jl
@@ -10,26 +10,26 @@ include("Plotting.jl")
 
 ############# SOURCES #######################
 filepath="../data/a03.csv"
-#data = readtable("set-a/set-a-text/a01.csv") # data
-#t = linspace(0.0, 60.0, n) # time vector
-
 
 ############ LOAD DATA ######################
 #----------- Read and fix data --------------
 (t,AECG) = process_svs(filepath)
 (n,m) = size(AECG) # m - number of electros, n - sample size
 
-# Notch Filtering and detrending
+
+########### PREPROCESING ####################
+#------- Notch Filtering and detrending ------------
 (AECG, lowSignal) = notch_filter(AECG);
 
 
 ########## SOURCE SEPARATION ################
 #----------------- ICA ----------------------
-k = 4 # number of components
+k = 2 # number of components
 (AECG_white) = MakeICAll(AECG)
 #------------ Sort ICA results ----------------------
 (AECG_sort)=SortICA(AECG_white)
 
 
-############### Plotting ###################
+############### PLOTTING ###################
+seconds=1 #seconds to plot
 Plotting(AECG,AECG_white,AECG_sort)
