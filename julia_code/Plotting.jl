4dbb954a8d544f5cf476936ccd7b2c53caa7ceb4
jaruanob <josue.ruanob@gmail.com>
2016-09-26 12:45:13 -0500

    Nothing important

--- julia_code/Plotting.jl
+++ julia_code/Plotting.jl
@@ -8,21 +8,21 @@ PyPlot.close("all")
 figure(1)
 for ind in 1:m
 subplot("41$(ind)")
-title("AECG data")
+if ind == 1 title("AECG data") end
  plot(t[1:win], AECG[1:win,ind], color="red", linewidth=1.0, linestyle="-")
 end
 
 figure(2)
 for ind in 1:k
   subplot("$(k)1$(ind)")
-  title("ICA_white $k")
+  if ind == 1 title("ICA_white") end
   plot(t[1:win], AECG_white[1:win,ind], color="black", linewidth=1.0, linestyle="-")
 end
 
 figure(3)
 for ind in 1:k
   subplot("$(k)1$(ind)")
-title("ICA_white")
+if ind == 1 title("ICA_sort") end
 plot(t[1:win], AECG_sort[1:win,ind], color="blue", linewidth=1.0, linestyle="-")
 end
 
