f28675b42f177359e532a0e5e3706f2c624b5cf9
jaruanob <josue.ruanob@gmail.com>
2016-09-27 18:09:58 -0500

    I achieved to interpolate the signal with Bspline, but with fourier, nop :(

--- julia_code/InterpSignal.jl
+++ julia_code/InterpSignal.jl
@@ -1,27 +1,29 @@
-using Interpolations
+using Interpolations, DataFrames, ArrayFire
 N=9;
 
 srand(0);
+ #a = @data([1.5, NA, NA, NA, NA, NA, 10.5]);
+ a = ([1.5, 3, 6, 10.5]);
+  b = linspace(1, 100, 10);
 f=rand(N);
 t = linspace(0.0, 1, N) # time vector
 
 
 ########### BSPLINE ############################
 
-x = interpolate(f,BSpline(Constant()), OnCell())
+x = interpolate(b,BSpline(Linear()), OnGrid())
+B=zeros(length(b)*2)
+for i in 0:length(b)-1
+  tmpidxs = i+1.5
+  B[i*2+1] = b[i+1]
+  B[i*2+2] = x[tmpidxs]
+end
 
-A = rand(20)
-A_x = collect(1.0:2.0:40.0)
-knots = (A_x,)
-itp = interpolate(knots, A, Gridded(Linear()))
-println(itp)
-itp[1.0]
-itp[2.0]
-itp[3.0]
-itp[4.0]
-
-########## FOURIER ###########################3
-
+v1 = x[0.6]
+v2 = x[3.6]
+#
+# ########## FOURIER ###########################3
+#
 # # compute FFT (the factor of N is just a normalization)
 # tildef=fft(f)/N;
 #
@@ -33,4 +35,15 @@ itp[4.0]
 # Sum
 # println(Sum)
 #
-# recover=ifftshift(Sum)
+# recover=real(ifft(Sum))
+#  #####################
+#
+
+#  a = @data([1.5, NA, NA, NA, NA, NA, 10.5]);
+#  b=collect(linspace(a[1],a[end],sum(isna(a))))
+#
+# idxs = find(~isna(a))
+# for i in 1:length(idxs)-1
+#   tmpidxs = idxs[i]:idxs[i+1]
+#   a[idxs[i]+1:idxs[i+1]-1] = linspace(a[idxs[i]],a[idxs[i+1]],length(tmpidxs))[2:end-1]
+# end
