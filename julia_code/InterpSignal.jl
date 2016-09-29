316981a563db2da74639513662c940550820c48c
jaruanob <josue.ruanob@gmail.com>
2016-09-29 13:41:54 -0500

    Resamplig with fourier, with high computational cost

--- julia_code/InterpSignal.jl
+++ julia_code/InterpSignal.jl
@@ -1,4 +1,4 @@
-using MultivariateStats, Base.Test, DataFrames, PyPlot, DSP
+using MultivariateStats, Base.Test, DataFrames, PyPlot, Multirate
 cd("/home/jarb/NI-Fecg/julia_code")
 
 include("process_svs.jl")
@@ -81,24 +81,24 @@ k = 4 # number of components
 ###############################################################33
 Fe = 1000; #Sample rate
 Te = 1/Fe;
-Nech = 60000; #number of samples
+Nech = 1000; #number of samples
 j =0 + 1im
 
 time = ([0:Te:(Nech-1)*Te]);
 timeDiscrete = ([0:1:Nech-1]')';
 frequency = (timeDiscrete/Nech)*Fe;
 
-signal = AECG_white[:,1]#linspace(1, 100, Nech);
+signal = AECG_white[1:Nech,1]#linspace(1, 100, Nech);
 spectrum =  signal*exp(-2*pi*j*timeDiscrete'*timeDiscrete/Nech);
 fspec = [0:Nech-1]*Fe/Nech;
 reconstruction = spectrum*exp(2*pi*j*timeDiscrete'*timeDiscrete/Nech)/Nech;
 
 figure(4)
-subplot(121)
+subplot(311)
 title("Signal")
-plot(t,signal)
-subplot(122)
-plot(t,real(reconstruction),color="red")
+plot(time,signal)
+subplot(312)
+plot(time,real(reconstruction),color="red")
 
 # **** interpolation ****
 
@@ -106,7 +106,22 @@ Finterp = 2*Fe;
 Tinterp = 1/Finterp;
 TimeInterp = [0:Tinterp:(Nech-1)*Te];
 NechInterp = length(TimeInterp);
-TimeInterpDiscrete = [0:NechInterp-1];
+TimeInterpDiscrete = ([0:NechInterp-1]')';
 
 #Compute original signal value without any interpolation
-signalResampled = cos(2*pi*F1*(TimeInterp))+cos(2*pi*F2*(TimeInterp))+cos(2*pi*FMax*(TimeInterp));
+#signalResampled = cos(2*pi*F1*(TimeInterp))+cos(2*pi*F2*(TimeInterp))+cos(2*pi*FMax*(TimeInterp));
+
+#Compute original signal interpolation by padding the fft and performing inverse fft on the result
+Nz = NechInterp-Nech;
+p1=zeros(1,convert(Int64,floor(Nz/2)))
+p2=fftshift(spectrum)
+p3=zeros(1,convert(Int64,floor(Nz/2))+rem(Nz,2))
+
+padded_spectrum0 = ifftshift([p1 p2' p3]);
+fspecPadded = [0:NechInterp-1]*Finterp/NechInterp;
+
+padded_reconstruction = padded_spectrum0*exp(2*pi*j*TimeInterpDiscrete*TimeInterpDiscrete'/NechInterp)/(1*Nech);
+#spectrumresampled = signalResampled*exp(-2*pi*j*TimeInterpDiscrete'*TimeInterpDiscrete/NechInterp);
+#fresampled = [0:NechInterp-1]*Fe/NechInterp;
+subplot(313)
+plot(time,real(reconstruction),color="black")
