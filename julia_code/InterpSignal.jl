2aef2a0227568dcec53d905f2b8a9047ef07a8d0
jaruanob <josue.ruanob@gmail.com>
2016-09-29 18:03:15 -0500

    Miss some details

--- julia_code/InterpSignal.jl
+++ julia_code/InterpSignal.jl
@@ -1,127 +1,62 @@
-using MultivariateStats, Base.Test, DataFrames, PyPlot, Multirate
-cd("/home/jarb/NI-Fecg/julia_code")
+function InterpSignal(AECG_white)
 
-include("process_svs.jl")
-include("Notch_Filter_Detrent.jl")
-include("MakeICAll.jl")
-include("SortICA.jl")
-include("Plotting.jl")
-
-
-############# SOURCES #######################
-filepath="../data/a01.csv"
-
-############ LOAD DATA ######################
-#----------- Read and fix data --------------
-(t,AECG) = process_svs(filepath)
-(n,m) = size(AECG) # m - number of electros, n - sample size
-
-
-########### PREPROCESING ####################
-#------- Notch Filtering and detrending ------------
-(AECG, lowSignal) = notch_filter(AECG);
-
-
-########## SOURCE SEPARATION ################
-#----------------- ICA ----------------------
-k = 4 # number of components
-(AECG_white) = MakeICAll(AECG)
-#------------ Sort ICA results ----------------------
-(AECG_sort)=SortICA(AECG_white)
-# N=9;
-#
-# srand(0);
-#  #a = @data([1.5, NA, NA, NA, NA, NA, 10.5]);
-#  a = ([1.5, 3, 6, 10.5]);
-#   b = linspace(1, 100, 10);
-# f=rand(N);
-# t = linspace(0.0, 1, N) # time vector
-#
-#
-# ########### BSPLINE ############################
-#
-# x = interpolate(b,BSpline(Linear()), OnGrid())
-# B=zeros(length(b)*2)
-# for i in 0:length(b)-1
-#   tmpidxs = i+1.5
-#   B[i*2+1] = b[i+1]
-#   B[i*2+2] = x[tmpidxs]
-# end
-#
-# v1 = x[0.6]
-# v2 = x[3.6]
-#
-# ########## FOURIER ###########################3
-#
-# # compute FFT (the factor of N is just a normalization)
-# tildef=fft(f)/N;
-#
-# # define the trigonometric interpolant defined by the equation above
-# Sum=0.0 + 0.0im;
-# for nu=0:N-1
-# Sum+=tildef[nu+1]*exp(2*pi*im*nu*t/N);
-# end
-# Sum
-# println(Sum)
-#
-# recover=real(ifft(Sum))
-#  #####################
-#
-
-#  a = @data([1.5, NA, NA, NA, NA, NA, 10.5]);
-#  b=collect(linspace(a[1],a[end],sum(isna(a))))
-#
-# idxs = find(~isna(a))
-# for i in 1:length(idxs)-1
-#   tmpidxs = idxs[i]:idxs[i+1]
-#   a[idxs[i]+1:idxs[i+1]-1] = linspace(a[idxs[i]],a[idxs[i+1]],length(tmpidxs))[2:end-1]
-# end
-
-
-###############################################################33
 Fe = 1000; #Sample rate
 Te = 1/Fe;
-Nech = 1000; #number of samples
-j =0 + 1im
-
+Nech = 60000; #number of samples
+AECG_resample=[]
 time = ([0:Te:(Nech-1)*Te]);
-timeDiscrete = ([0:1:Nech-1]')';
-frequency = (timeDiscrete/Nech)*Fe;
+new_size=n*fact;
 
-signal = AECG_white[1:Nech,1]#linspace(1, 100, Nech);
-spectrum =  signal*exp(-2*pi*j*timeDiscrete'*timeDiscrete/Nech);
-fspec = [0:Nech-1]*Fe/Nech;
-reconstruction = spectrum*exp(2*pi*j*timeDiscrete'*timeDiscrete/Nech)/Nech;
+for i in 1:m
 
-figure(4)
-subplot(311)
-title("Signal")
-plot(time,signal)
-subplot(312)
-plot(time,real(reconstruction),color="red")
+signal=(AECG_white[:,i]')';
 
-# **** interpolation ****
 
-Finterp = 2*Fe;
-Tinterp = 1/Finterp;
-TimeInterp = [0:Tinterp:(Nech-1)*Te];
-NechInterp = length(TimeInterp);
-TimeInterpDiscrete = ([0:NechInterp-1]')';
+if new_size > n
+    incr = 1;
+else
+    if new_size==0
+       signal_resmp=[]
+       return
+    end
+    incr = floor(n/new_size) + 1;
+    new_size = incr*new_size;
+end
 
-#Compute original signal value without any interpolation
-#signalResampled = cos(2*pi*F1*(TimeInterp))+cos(2*pi*F2*(TimeInterp))+cos(2*pi*FMax*(TimeInterp));
+ a = fft(signal,1);
+ nyqst = ceil((n+1)/2);
+ p1=a[1:nyqst,:]
+ p2=zeros(new_size-n,1)
+ p3=a[nyqst+1:n,:]
+ b = [p1' p2' p3']'
+ if rem(1,2) == 0
+    b[nyqst,:] = b[nyqst,:]/2
+    b[nyqst+new_size-n,:] = b[nyqst,:]
+ end
 
-#Compute original signal interpolation by padding the fft and performing inverse fft on the result
-Nz = NechInterp-Nech;
-p1=zeros(1,convert(Int64,floor(Nz/2)))
-p2=fftshift(spectrum)
-p3=zeros(1,convert(Int64,floor(Nz/2))+rem(Nz,2))
 
-padded_spectrum0 = ifftshift([p1 p2' p3]);
-fspecPadded = [0:NechInterp-1]*Finterp/NechInterp;
+ Finterp = 2*Fe;
+ Tinterp = 1/Finterp;
+ t_interp = [0:Tinterp:(Nech-1)*Te];
+ signal_resmp = ifft(b,1);
+ if isreal(signal)
+    signal_resmp = real(signal_resmp)
+ end
+ signal_resmp = signal_resmp * new_size / n;
+ signal_resmp = signal_resmp[1:incr:new_size-1,:]  #% Skip over extra points when oldny <= 1.
 
-padded_reconstruction = padded_spectrum0*exp(2*pi*j*TimeInterpDiscrete*TimeInterpDiscrete'/NechInterp)/(1*Nech);
-#spectrumresampled = signalResampled*exp(-2*pi*j*TimeInterpDiscrete'*TimeInterpDiscrete/NechInterp);
-#fresampled = [0:NechInterp-1]*Fe/NechInterp;
-subplot(313)
-plot(time,real(reconstruction),color="black")
+AECG_resample[i,:] = signal_resmp
+end
+
+
+
+# figure(1)
+# subplot(211)
+# title("Signal")
+# plot(time,signal)
+# subplot(212)
+# plot(t_interp,real(signal_resmp),color="red")
+
+return AECG_resample
+
+end
