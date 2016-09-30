80fd7c37edc160f7f6c0a4f44f2130ecdf4908c6
jaruanob <josue.ruanob@gmail.com>
2016-09-30 15:56:40 -0500

    Function complete: this resampling the signal according to fact variable, as scalingrate factor

--- julia_code/InterpSignal.jl
+++ julia_code/InterpSignal.jl
@@ -3,8 +3,15 @@ function InterpSignal(AECG_white)
 Fe = 1000; #Sample rate
 Te = 1/Fe;
 Nech = 60000; #number of samples
-AECG_resample=[]
+AECG_resample=zeros(120000,4)
+signal_resmp=[]
 time = ([0:Te:(Nech-1)*Te]);
+
+Finterp = 2*Fe;
+Tinterp = 1/Finterp;
+t_interp = [0:Tinterp:(Nech)*Te];
+t_interp=t_interp[1:120000,1]
+
 new_size=n*fact;
 
 for i in 1:m
@@ -34,29 +41,30 @@ end
     b[nyqst+new_size-n,:] = b[nyqst,:]
  end
 
-
- Finterp = 2*Fe;
- Tinterp = 1/Finterp;
- t_interp = [0:Tinterp:(Nech-1)*Te];
  signal_resmp = ifft(b,1);
  if isreal(signal)
     signal_resmp = real(signal_resmp)
  end
  signal_resmp = signal_resmp * new_size / n;
- signal_resmp = signal_resmp[1:incr:new_size-1,:]  #% Skip over extra points when oldny <= 1.
+ signal_resmp = signal_resmp[1:incr:new_size,:]  #% Skip over extra points when oldny <= 1.
+size(signal_resmp)
 
-AECG_resample[i,:] = signal_resmp
+AECG_resample[:,i] = signal_resmp
+
+
+
+# if i== 1
+#   PyPlot.close("all")
+# end
+# figure("$(i)")
+#   subplot(211)
+#   title("Signal")
+#   plot(time,signal)
+#   subplot(212)
+#   plot(t_interp,(AECG_resample[:,i]),color="red")
+
 end
 
-
-
-# figure(1)
-# subplot(211)
-# title("Signal")
-# plot(time,signal)
-# subplot(212)
-# plot(t_interp,real(signal_resmp),color="red")
-
-return AECG_resample
+return t_interp, AECG_resample
 
 end
