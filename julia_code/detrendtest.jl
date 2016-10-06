c0d1d25c1fba3e507c2d091f959db49984bbf980
bioalmeida21 <jaalmeidam@unal.edu.co>
2016-10-06 16:34:05 -0500

    Median Filter

--- julia_code/detrendtest.jl
+++ julia_code/detrendtest.jl
@@ -11,7 +11,7 @@ csvdata=readcsv(string(ECGfiles, "/a23.csv"));
 ecgtime = csvdata[3:end,1];
 signals = csvdata[3:end,2:end];
 
-responsetype = Lowpass(3, fs=1000);
+responsetype = Lowpass(3,fs=1000);
 prototype=Butterworth(8);
 
 detrend_filter= digitalfilter(responsetype, prototype);
