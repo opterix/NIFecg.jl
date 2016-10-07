ac0fdbea4493a2e03fa4e9f4547c11018b29361e
jaruanob <josue.ruanob@gmail.com>
2016-10-07 18:01:34 -0500

    First approach of the QRS's mother dector

--- julia_code/QRSm_detector.jl
+++ julia_code/QRSm_detector.jl
@@ -1,66 +1,91 @@
 
-function QRSm_detector(signal)
+function QRSm_detector(signal_mother)
 
-threshold=0.2
+
 
 #for i in 1:m
-signal = signal[1:num_sample,1]
-
+global signal = signal_mother[1:num_sample,1]
 max_value = maximum(signal)
 min_value = minimum(signal)
-#
-escale_signal = []#zeros(num_sample,1)
-point_temp = zeros(num_sample,200)
-pos_temp = zeros(num_sample,200)
-QRSm_pos = ones(1,200)*NaN
-QRSm_value = ones(1,200)*NaN
-#
-acum=0
-acum=0
 
-indx=1
-indx=1
+global escale_signal = []#zeros(num_sample,1)
+global threshold=0.2
+global point_temp_max = zeros(num_sample,2000)
+global pos_temp_max = zeros(num_sample,2000)
+global point_temp_min = zeros(num_sample,2000)
+global pos_temp_min = zeros(num_sample,2000)
+global QRSm_pos_temp = ones(2,2000)*NaN
+global QRSm_value_temp = ones(2,2000)*NaN
+#global QRSm_pos = ones(2,2000)*NaN
+#global QRSm_value = ones(2,2000)*NaN
+global i
+
+
+acum_max = 0
+acum_min = 0
+indx_max = 1
+indx_min = 1
+
 
 for i in 1:num_sample
  escale_signal = (signal[i,1]-min_value)/(max_value-min_value)
+#------ max peaks -------------------
+(acum_max,indx_max)= max_points(acum_max,indx_max)
 
-############## MAX #############
-if escale_signal >= (1-threshold)
-   acum=acum+1
-   point_temp[acum,indx] = signal[i,1]
-   pos_temp[acum,indx] = i/1000
+#------ min peaks -------------------
+(acum_min,indx_min)= min_points(acum_min,indx_min)
+end
+
+if indx_min > indx_max
+   QRSm_pos = QRSm_pos_temp[1,1:indx_max]
+   QRSm_value = QRSm_value_temp[1,1:indx_max]
 else
-  if acum != 0
-    value_temp , pos= findmax( point_temp[:,indx] )
-    QRSm_pos[1,indx] = pos_temp[pos,indx]
-    QRSm_value[1,indx] =  value_temp
-    indx=indx+1
-  end
-   acum=0
+  QRSm_pos = QRSm_pos_temp[2,1:indx_min]
+  QRSm_value = QRSm_value_temp[2,1:indx_min]
 end
-
-# ############## MIN #############
-# if escale_signal <= (threshold)
-#    acum=acum+1
-#    point_temp[acum,indx] = signal[i,1]
-#    pos_temp[acum,indx] = i/1000
-# else
-#   if acum != 0
-#     value_temp , pos= findmin( point_temp[:,indx] )
-#     QRSm_pos[2,indx] = pos_temp[pos,indx]
-#     QRSm_value[2,indx] =  value_temp
-#     indx=indx+1
-#   end
-#    acum=0
-# end
-
-
-end
-
 
 return QRSm_pos , QRSm_value
 
 end
+
+
+########################################
+function max_points(acum,indx)
+  if escale_signal >= (1-threshold)
+     acum=acum+1
+     point_temp_max[acum,indx] = signal[i,1]
+     pos_temp_max[acum,indx] = i/rate_sample
+  else
+    if acum != 0
+      value_temp , pos= findmax( point_temp_max[:,indx] )
+      QRSm_pos_temp[1,indx] = pos_temp_max[pos,indx]
+      QRSm_value_temp[1,indx] =  value_temp
+      indx=indx+1
+    end
+     acum=0
+  end
+  return acum, indx
+end
+
+#########################################
+function min_points(acum,indx)
+  if escale_signal <= (threshold)
+      acum=acum+1
+      point_temp_min[acum,indx] = signal[i,1]
+      pos_temp_min[acum,indx] = i/rate_sample
+   else
+     if acum != 0
+       value_temp , pos= findmin( point_temp_min[:,indx] )
+       QRSm_pos_temp[2,indx] = pos_temp_min[pos,indx]
+       QRSm_value_temp[2,indx] =  value_temp
+       indx=indx+1
+     end
+      acum=0
+   end
+   return acum, indx
+end
+
+
 
 # point = point_temp[1:acum,1]
 # #sorting_values=sort(signal,1, rev=true)
