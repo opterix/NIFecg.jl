033725480f0ca4aa2a923a304024966bbb91da80
jaruanob <josue.ruanob@gmail.com>
2016-11-08 16:37:41 -0500

    Convert to function

--- julia_code/Font_Separation_SVD.jl
+++ julia_code/Font_Separation_SVD.jl
@@ -1,3 +1,7 @@
+function Font_Separation_SVD(signal)
+
+#signal=AECG_fnotch
+
 ## SINGULAR VALUE DECOMPOSITION
 
 ## Defino la ventana a trabajar de 200 ms
@@ -16,13 +20,13 @@ Win_Pos_Fw_Int = convert(Array{Int64}, round(Win_Pos_Fw));
 
 ## Tomo los valores alrededor del complejo R en la ventana definida
 
-SVD_Values=AECG_fnotch[Win_Pos_Rw_Int[1]:Win_Pos_Fw_Int[1]];
+SVD_Values=signal[Win_Pos_Rw_Int[1]:Win_Pos_Fw_Int[1]];
 SVD_Values_Mot = zeros(length(SVD_Values), length(QRSm_pos));
 
 
 for i = 1:length(QRSm_pos)   ##Guardo los valores de los complejos R detectados en una matriz
 
-	SVD_Values_Mot[1:length(SVD_Values),i]=AECG_fnotch[Win_Pos_Rw_Int[i]:Win_Pos_Fw_Int[i]];
+	SVD_Values_Mot[1:length(SVD_Values),i]=signal[Win_Pos_Rw_Int[i]:Win_Pos_Fw_Int[i]];
 end
 
 # Aplico Singular Value Decomposition a la matriz que contiene los R.
@@ -37,10 +41,7 @@ NUrec=U*diagm(Sprima)*V'
 
 return NUrec,SVD_Values_Mot
 
-
-
-
-
+end
 
 ## Ventana para tomar los valores y armar la matriz 100ms
 
