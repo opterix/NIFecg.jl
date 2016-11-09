e7df9813e5fc934b6e9327bcb83429d8e7a33ccb
jaruanob <josue.ruanob@gmail.com>
2016-11-09 18:35:12 -0500

    The SVD function now apply all channels, and reconstruct the signal with SVD result (asign zeros in positions out of windows of SVD process)

--- julia_code/Font_Separation_SVD.jl
+++ julia_code/Font_Separation_SVD.jl
@@ -1,49 +1,45 @@
 function Font_Separation_SVD(signal)
 
-#signal=AECG_fnotch
+window_svd=200; #samples
+signal_rec=zeros(n,m);
 
 ## SINGULAR VALUE DECOMPOSITION
 
 ## Defino la ventana a trabajar de 200 ms
 
-(Win_Pos_Fw) = (QRSm_pos*1000) + 100 ;
-(Win_Pos_Rw) = (QRSm_pos*1000) - 100 ;
+(Win_Pos_Fw) = (QRSm_pos*rate_sample) + window_svd/2 ;
+(Win_Pos_Rw) = (QRSm_pos*rate_sample) - window_svd/2 ;
 
 ## Convierto el arreglo a enteros
-
-## Win_Pos_Rw_Int = Array(Int64, length(QRSm_pos), 1)
-## Win_Pos_Fw_Int = Array(Int64, length(QRSm_pos), 1)
 
 Win_Pos_Rw_Int = convert(Array{Int64}, round(Win_Pos_Rw));
 Win_Pos_Fw_Int = convert(Array{Int64}, round(Win_Pos_Fw));
 
 
-## Tomo los valores alrededor del complejo R en la ventana definida
+for i = 1:m #aplicar proceso para cada canal
+	## Tomo los valores alrededor del complejo R en la ventana definida
+	SVD_Values = zeros(window_svd+1, length(QRSm_pos));
 
-SVD_Values=signal[Win_Pos_Rw_Int[1]:Win_Pos_Fw_Int[1]];
-SVD_Values_Mot = zeros(length(SVD_Values), length(QRSm_pos));
+	##Guardo los valores de los complejos R detectados en una matriz
+	for ii = 1:length(QRSm_pos)   
+		SVD_Values[1:window_svd+1,ii]=signal[Win_Pos_Rw_Int[ii]:Win_Pos_Fw_Int[ii],i];
+	end
 
+	# Aplico Singular Value Decomposition a la matriz que contiene los R.
 
-for i = 1:length(QRSm_pos)   ##Guardo los valores de los complejos R detectados en una matriz
+	(U,S,V)=svd(SVD_Values);
 
-	SVD_Values_Mot[1:length(SVD_Values),i]=signal[Win_Pos_Rw_Int[i]:Win_Pos_Fw_Int[i]];
+	Sprima=zeros(length(QRSm_pos));
+	Sprima[1:3]=S[1:3];
+	#Sprima=S;
+	NUrec=U*diagm(Sprima)*V';
+	
+	#Reconstruir cada canal
+	for ii = 1:length(QRSm_pos)  
+		signal_rec[Win_Pos_Rw_Int[ii]:Win_Pos_Fw_Int[ii],i]=NUrec[1:window_svd+1,ii];
+	end
 end
 
-# Aplico Singular Value Decomposition a la matriz que contiene los R.
-
-
-(U,S,V)=svd(SVD_Values_Mot);
-
-Sprima=zeros(length(QRSm_pos))
-Sprima[1:3]=S[1:3]
-#Sprima=S;
-NUrec=U*diagm(Sprima)*V'
-
-return NUrec,SVD_Values_Mot
+return signal_rec;
 
 end
-
-## Ventana para tomar los valores y armar la matriz 100ms
-
-
-
