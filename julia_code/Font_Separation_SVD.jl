970e6818d8456100d794604b453b6747e21c0260
jaruanob <josue.ruanob@gmail.com>
2016-11-10 12:58:21 -0500

    The function apply for all channels and subtract from the original signals

--- julia_code/Font_Separation_SVD.jl
+++ julia_code/Font_Separation_SVD.jl
@@ -1,7 +1,6 @@
 function Font_Separation_SVD(signal)
 
 window_svd=200; #samples
-signal_rec=zeros(n,m);
 
 ## SINGULAR VALUE DECOMPOSITION
 
@@ -10,11 +9,17 @@ signal_rec=zeros(n,m);
 (Win_Pos_Fw) = (QRSm_pos*rate_sample) + window_svd/2 ;
 (Win_Pos_Rw) = (QRSm_pos*rate_sample) - window_svd/2 ;
 
+## Pesos de la ventana (Tukey)
+alpha=0.5; #constante 0(rectangular) - 1(gausiana)
+win_weight=tukey(window_svd+1, alpha)*0.8+0.2;
+
 ## Convierto el arreglo a enteros
 
 Win_Pos_Rw_Int = convert(Array{Int64}, round(Win_Pos_Rw));
 Win_Pos_Fw_Int = convert(Array{Int64}, round(Win_Pos_Fw));
 
+## Inicializando variables
+signal_rec=zeros(n,m);
 
 for i = 1:m #aplicar proceso para cada canal
 	## Tomo los valores alrededor del complejo R en la ventana definida
@@ -22,9 +27,10 @@ for i = 1:m #aplicar proceso para cada canal
 
 	##Guardo los valores de los complejos R detectados en una matriz
 	for ii = 1:length(QRSm_pos)   
-		SVD_Values[1:window_svd+1,ii]=signal[Win_Pos_Rw_Int[ii]:Win_Pos_Fw_Int[ii],i];
+		SVD_Values[1:window_svd+1,ii]=(signal[Win_Pos_Rw_Int[ii]:Win_Pos_Fw_Int[ii],i]).*win_weight;
+		
 	end
-
+	
 	# Aplico Singular Value Decomposition a la matriz que contiene los R.
 
 	(U,S,V)=svd(SVD_Values);
@@ -34,12 +40,15 @@ for i = 1:m #aplicar proceso para cada canal
 	#Sprima=S;
 	NUrec=U*diagm(Sprima)*V';
 	
-	#Reconstruir cada canal
+	#Reconstruir cada canal 
 	for ii = 1:length(QRSm_pos)  
 		signal_rec[Win_Pos_Rw_Int[ii]:Win_Pos_Fw_Int[ii],i]=NUrec[1:window_svd+1,ii];
 	end
 end
 
-return signal_rec;
+# Restar con la original
+signal_subtract=signal-signal_rec;
+
+return signal_rec, signal_subtract;
 
 end
