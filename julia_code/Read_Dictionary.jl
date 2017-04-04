60dec0ffecf2a374ab5c9db6686bd6f4c32cf8f8
bioalmeida21 <jaalmeidam@unal.edu.co>
2017-04-04 11:12:30 -0500

    Archivo para leer y concatenar los diccionarios

---
+++ julia_code/Read_Dictionary.jl
@@ -0,0 +1,20 @@
+#### Leo los diccionarios generados para la descomposición Wavelet
+
+
+## Recursos ##
+
+data_dictio="../Dictionaries"
+list_file=readdir(data_dictio)
+num_files=size(list_file,1);
+B=load(list_file[1]);
+
+Mat_To_Wavelet_Pos=B["pos_examples"];
+Mat_To_Wavelet_Neg=B["neg_examples"];
+
+for i in 2:num_files
+    file_name = list_file[i];
+    println("Procesando Dictionary $(file_name)")
+    d = load(file_name)
+    Mat_To_Wavelet_Pos=vcat(Mat_To_Wavelet_Pos,d["pos_examples"]);
+    Mat_To_Wavelet_Neg=vcat(Mat_To_Wavelet_Neg,d["neg_examples"]);
+end
