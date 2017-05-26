a4bc1f11791f7d5301f07ea197134a929ddd0be8
jaruanob <josue.ruanob@gmail.com>
2017-05-26 15:58:11 -0500

    Cambio de nombre

---
+++ MaternalFetalMonitor/src/MaternalFetalMonitor.jl
@@ -0,0 +1,26 @@
+VERSION >= v"0.1" && __precompile__()
+
+module MaternalFetalMonitor
+
+	#---------------------------------- LIBRARIES 
+	using Reexport
+
+	#------------------------------------ MODULES 
+	include("loadDataModule.jl")
+	include("preProcessingModule.jl")
+	include("motherSubstractionModule.jl")
+	include("fetalSubstractionModule.jl")
+	include("plotModule.jl")
+
+	@reexport using .loadDataModule
+	@reexport using	.preProcessingModule
+	@reexport using	.motherSubstractionModule
+	@reexport using	.fetalSubstractionModule
+	@reexport using	.plotModule
+
+
+	#----------------------- ADDITIONAL FUNCTIONS
+	include("runTest.jl"); export sourceSeparationTest
+	include("groupVar.jl"); export groupVar
+
+end
---
+++ MaternalFetalMonitor/src/runTest.jl
@@ -0,0 +1,33 @@
+function materFetMonitorTest(filename,ts,sr)
+
+	#------------------------------------ GLOBAL VARIABLES 
+	# ts = 60 # time_signal in seconds
+	# sr = 1000 # sample rate
+	ns = ts * sr # number of samples
+
+	#------------------------------------------- LOAD DATA
+	(nch,t,AECG,fetal_annot) = loadData(filename,ns)
+
+	#---------------------------------------- PREPROCESING
+	(AECG_clean) = preProcessing(AECG,sr)
+
+	#----------------- MOTHER SUBSTRACTION AND COMPUTATION
+	(heart_rate_mother,AECGm_ica,SVDrec,AECGm_sort,AECG_residual,QRSm_pos,QRSm_value) = motherSubstraction(AECG_clean,nch,sr,ns,ts)
+
+	#------------------ FETAL SUBSTRACTION AND COMPUTATION 
+	(AECGf_sort,QRSf_pos,QRSf_value,QRSfcell_pos,QRSfcell_value,heart_rate_feto, QRSfcell_pos_smooth, SMI, gini_measure) = fetalSubstraction(AECG_residual,heart_rate_mother,nch,sr,ts)
+
+	#---------- Grouping variables 
+	(inputVar,motherVar,fetalVar)=groupVar(
+	nch,ns,ts,sr,
+	t,fetal_annot,AECG,AECG_clean,
+	AECGm_ica,AECGm_sort,AECG_residual,QRSm_pos,QRSm_value,heart_rate_mother,SVDrec,
+	AECGf_sort,QRSf_pos,QRSf_value,QRSfcell_pos,QRSfcell_value,heart_rate_feto, QRSfcell_pos_smooth, SMI, gini_measure)
+
+	return inputVar,motherVar,fetalVar
+
+end
+
+
+
+
