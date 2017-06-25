9f7fbec8b863db3b979b2c97570bcbfa707de9b5
opterix <daedrobu87@gmail.com>
2017-06-24 23:51:15 -0500

    Cambios scripts bash para correr experimentos

--- julia_code/RF_testFV16X1.sh
+++ julia_code/RF_testFV16X1.sh
@@ -2,11 +2,11 @@
 
 #Lanzar de a 3 procesos!!
 julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees10_dwt_levels5.jld > logs/resRF_dwt5_exp1.txt &
-julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees50_dwt_levels5.jld > logs/resRF_dwt5_exp2.txt &
-julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees150_dwt_levels5.jld > logs/resRF_dwt5_exp3.txt 
+#julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees50_dwt_levels5.jld > logs/resRF_dwt5_exp2.txt &
+#julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees150_dwt_levels5.jld > logs/resRF_dwt5_exp3.txt 
 julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees10_dwt_levels6.jld > logs/resRF_dwt6_exp1.txt &
-julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees50_dwt_levels6.jld > logs/resRF_dwt6_exp2.txt &
-julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees150_dwt_levels6.jld > logs/resRF_dwt6_exp3.txt 
+#julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees50_dwt_levels6.jld > logs/resRF_dwt6_exp2.txt &
+#julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees150_dwt_levels6.jld > logs/resRF_dwt6_exp3.txt 
 julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees10_dwt_levels7.jld > logs/resRF_dwt7_exp1.txt &
-julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees50_dwt_levels7.jld > logs/resRF_dwt7_exp2.txt &
-julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees150_dwt_levels7.jld > logs/resRF_dwt7_exp3.txt  
+#julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees50_dwt_levels7.jld > logs/resRF_dwt7_exp2.txt &
+#julia RF_Model_Test_Wavelets.jl ../models/RFmodel_Ch1_16_randFeats4_nTrees150_dwt_levels7.jld > logs/resRF_dwt7_exp3.txt  
