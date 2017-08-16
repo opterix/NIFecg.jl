cbc8a69d5963f736ae51f95320133519319e2ae3
jaruanob <josue.ruanob@gmail.com>
2017-08-16 18:02:08 -0500

    Agregando función de medir por ventanas

--- src/test/runTestWindow.jl
+++ src/test/runTestWindow.jl
@@ -16,34 +16,35 @@ function MFMTestWindow(filename,ti,tf,sr,f)
 	ns = (tf-ti) * sr # number of samples
 
 	#------------------------------------------- LOAD DATA
+	(nch,t,AECG,fetal_annot) = loadData(filename,ns,ti,tf,f)	
 	(nsLimits)=divideSignal(AECG,ns,nch,f)
 	for i = 1 : f 	
-	b = nsLimits[i,1];
-	f = nsLimits[i,2];
+	bw = nsLimits[i,1];
+	fw = nsLimits[i,2];
 	nsd = nsLimits[i,2]-nsLimits[i,1];	
-	(nch,t,AECG,fetal_annot) = loadData(filename,nsd,sr,b,f)
+	(nch,td,AECG,fetal_annot) = loadData(filename,nsd,sr,bw,fw)
 
 	#---------------------------------------- PREPROCESING
 	(AECG_clean) = preProcessing(AECG,sr,nch,nsd)
 
 	#----------------- MOTHER SUBSTRACTION AND COMPUTATION
-	(heart_rate_mother,AECGm_ica,SVDrec,AECGm_sort,AECG_residual,QRSm_pos,QRSm_value) = motherSubstraction(AECG_clean,nch,sr,nsd,b,f)
+	(heart_rate_mother,AECGm_ica,SVDrec,AECGm_sort,AECG_residual,QRSm_pos,QRSm_value) = motherSubstraction(AECG_clean,nch,sr,nsd,bw,fw)
 
 	#------------------ FETAL SUBSTRACTION AND COMPUTATION 
-	(AECGf_sort,QRSf_pos,QRSf_value,QRSfcell_pos,QRSfcell_value,heart_rate_feto, QRSfcell_pos_smooth, SMI, gini_measure) = fetalSubstraction(AECG_residual,heart_rate_mother,nch,sr,ti,tf)
+	(AECGf_sort,QRSf_pos,QRSf_value,QRSfcell_pos,QRSfcell_value,heart_rate_feto, QRSfcell_pos_smooth, SMI, gini_measure) = fetalSubstraction(AECG_residual,heart_rate_mother,nch,sr,bw,fw)
 
 
 
 	(tAcum,fetal_annotAcum,AECGAcum,AECG_cleanAcum,
 AECG_icaAcum,SVDrecAcum,AECG_sortAcum,AECG_residualAcum,heart_rateAcum,QRS_valueAcum,
 QRS_posAcum,AECG_sortAcum,QRS_posAcum,QRS_valueAcum,QRScell_posAcum,QRScell_valueAcum,
-heart_rateAcum,QRScell_pos_smoothAcum,SMIAcum,gini_measureAcum) = initializationAcum(i,t,fetal_annot,AECG,AECG_clean,
+heart_rateAcum,QRScell_pos_smoothAcum,SMIAcum,gini_measureAcum) = initializationAcum(i,td,fetal_annot,AECG,AECG_clean,
 AECGm_ica,AECGm_sort,AECG_residual,QRSm_pos,QRSm_value,heart_rate_mother,SVDrec,
 AECGf_sort,QRSf_pos,QRSf_value,QRSfcell_pos,QRSfcell_value,heart_rate_feto, QRSfcell_pos_smooth,SMI,gini_measure)
 
 
 (tAcum,fetal_annotAcum,AECGAcum,AECG_cleanAcum,	AECGm_icaAcum,SVDrecAcum,AECGm_sortAcum,AECG_residualAcum,heart_rate_motherAcum,	QRSm_valueAcum,QRSm_posAcum,AECGf_sortAcum,QRSf_posAcum,QRSf_valueAcum,QRSfcell_posAcum,
-QRSfcell_valueAcum,heart_rate_fetoAcum,QRSfcell_pos_smoothAcum,SMIAcum,gini_measureAcum) = concatVar(i,t,fetal_annot,AECG,AECG_clean,
+QRSfcell_valueAcum,heart_rate_fetoAcum,QRSfcell_pos_smoothAcum,SMIAcum,gini_measureAcum) = concatVar(i,td,fetal_annot,AECG,AECG_clean,
 AECGm_ica,AECGm_sort,AECG_residual,QRSm_pos,QRSm_value,heart_rate_mother,SVDrec,
 AECGf_sort,QRSf_pos,QRSf_value,QRSfcell_pos,QRSfcell_value,heart_rate_feto, QRSfcell_pos_smooth,SMI,gini_measure,tAcum,fetal_annotAcum,AECGAcum,AECG_cleanAcum,	AECGm_icaAcum,SVDrecAcum,AECGm_sortAcum,AECG_residualAcum,heart_rate_motherAcum,	QRSm_valueAcum,QRSm_posAcum,AECGf_sortAcum,QRSf_posAcum,QRSf_valueAcum,QRSfcell_posAcum,
 QRSfcell_valueAcum,heart_rate_fetoAcum,QRSfcell_pos_smoothAcum,SMIAcum,gini_measureAcum)
