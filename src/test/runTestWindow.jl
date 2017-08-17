22015edfe87766bcf03e4dd30648b5af299b7a82
jaruanob <josue.ruanob@gmail.com>
2017-08-16 19:32:44 -0500

    Agregando función de medir por ventanas

--- src/test/runTestWindow.jl
+++ src/test/runTestWindow.jl
@@ -38,16 +38,20 @@ function MFMTestWindow(filename,ti,tf,sr,f)
 
 
 (AECGAcum,AECG_cleanAcum,	AECGm_icaAcum,SVDrecAcum,AECGm_sortAcum,AECG_residualAcum,heart_rate_motherAcum,	QRSm_valueAcum,QRSm_posAcum,AECGf_sortAcum,QRSf_posAcum,QRSf_valueAcum,QRSfcell_posAcum,
-QRSfcell_valueAcum,heart_rate_fetoAcum,QRSfcell_pos_smoothAcum,SMIAcum,gini_measureAcum) = initializationAcum(nch,f,i,AECG,AECG_clean,
+QRSfcell_valueAcum,heart_rate_fetoAcum,QRSfcell_pos_smoothAcum,SMIAcum,gini_measureAcum) = initializationAcum(nch,f,i,AECG,AECG_clean,AECGm_ica,AECGm_sort,AECG_residual,QRSm_pos,
+QRSm_value,heart_rate_mother,SVDrec,AECGf_sort,QRSf_pos,QRSf_value,QRSfcell_pos,
+QRSfcell_value,heart_rate_feto, QRSfcell_pos_smooth,SMI,gini_measure)
+
+
+(AECGAcum,AECG_cleanAcum,AECGm_icaAcum,SVDrecAcum,AECGm_sortAcum,AECG_residualAcum,
+heart_rate_motherAcum,QRSm_valueAcum,QRSm_posAcum,AECGf_sortAcum,QRSf_posAcum,
+QRSf_valueAcum,QRSfcell_posAcum,QRSfcell_valueAcum,heart_rate_fetoAcum,
+QRSfcell_pos_smoothAcum,SMIAcum,gini_measureAcum) = concatVar(i,AECGd,AECG_clean,
 AECGm_ica,AECGm_sort,AECG_residual,QRSm_pos,QRSm_value,heart_rate_mother,SVDrec,
-AECGf_sort,QRSf_pos,QRSf_value,QRSfcell_pos,QRSfcell_value,heart_rate_feto, QRSfcell_pos_smooth,SMI,gini_measure)
-
-
-	(AECGAcum,AECG_cleanAcum,AECGm_icaAcum,SVDrecAcum,AECGm_sortAcum,AECG_residualAcum,heart_rate_motherAcum,	QRSm_valueAcum,QRSm_posAcum,AECGf_sortAcum,QRSf_posAcum,QRSf_valueAcum,QRSfcell_posAcum,
-	QRSfcell_valueAcum,heart_rate_fetoAcum,QRSfcell_pos_smoothAcum,SMIAcum,gini_measureAcum) = concatVar(i,AECGd,AECG_clean,
-	AECGm_ica,AECGm_sort,AECG_residual,QRSm_pos,QRSm_value,heart_rate_mother,SVDrec,
-	AECGf_sort,QRSf_pos,QRSf_value,QRSfcell_pos,QRSfcell_value,heart_rate_feto, QRSfcell_pos_smooth,SMI,gini_measure,AECGAcum,AECG_cleanAcum,	AECGm_icaAcum,SVDrecAcum,AECGm_sortAcum,AECG_residualAcum,heart_rate_motherAcum,	QRSm_valueAcum,QRSm_posAcum,AECGf_sortAcum,QRSf_posAcum,QRSf_valueAcum,QRSfcell_posAcum,
-	QRSfcell_valueAcum,heart_rate_fetoAcum,QRSfcell_pos_smoothAcum,SMIAcum,gini_measureAcum)
+AECGf_sort,QRSf_pos,QRSf_value,QRSfcell_pos,QRSfcell_value,heart_rate_feto, QRSfcell_pos_smooth,SMI,gini_measure,AECGAcum,AECG_cleanAcum,AECGm_icaAcum,SVDrecAcum,
+AECGm_sortAcum,AECG_residualAcum,heart_rate_motherAcum,QRSm_valueAcum,QRSm_posAcum,
+AECGf_sortAcum,QRSf_posAcum,QRSf_valueAcum,QRSfcell_posAcum,QRSfcell_valueAcum,
+heart_rate_fetoAcum,QRSfcell_pos_smoothAcum,SMIAcum,gini_measureAcum)
 
 	end
 
