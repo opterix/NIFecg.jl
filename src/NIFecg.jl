a89e07fd0247b6e4c7945a2961e0c0f8f786c8e1
jaruanob <josue.ruanob@gmail.com>
2017-11-29 11:07:29 -0500

    Adicionando función de socket

--- src/NIFecg.jl
+++ src/NIFecg.jl
@@ -23,6 +23,7 @@ module NIFecg # Non Invasive Fetal electrocardiogram
 	include("test/runTestDBPlot.jl"); export MFMTestDBPlot
 	include("test/runTestWindow.jl"); export MFMTestWindow
 	include("test/runToJavascript.jl"); export MFMJavascript
+	include("runSocketTest.jl"); export MFMSocket
 	include("share/divideSignal.jl"); export divideSignal	
 	include("share/loadDataWindow.jl"); export loadDataWindow
 	include("share/concatVar.jl"); export concatVar
